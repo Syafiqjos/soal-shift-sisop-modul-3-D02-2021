104f85882012da6c3921286dac506c76d30c9ff1
Syafiqjos <syafiqjos@gmail.com>
2021-05-06 07:37:56 -0400

    soal1 - add akun settings login register

--- soal1/Server/server.c
+++ soal1/Server/server.c
@@ -7,8 +7,119 @@
 #include <stdbool.h>
 #define PORT 8080
 
+typedef struct {
+	char id[64];
+	char pass[64];
+} akun;
+
+typedef struct {
+	char path[64];
+	char publisher[64];
+	char year[64];
+} buku;
+
+FILE *akun_file;
+akun akun_data[10001] = {};
+int akun_data_size = 0;
+
+akun *logined_akun = NULL;
+
+FILE *buku_file;
+buku buku_data[10001] = {};
+int buku_data_size = 0
+
 void resetbuffer(char *s){
 	memset(s, 0, sizeof(char) * 1024);
+}
+
+void read_akun_file(){
+	char data_temp[256] = {};
+
+	int i = 0;
+
+	buku_file = fopen("files.tsv", "r");
+
+	printf("Reading files.tsv data..\n");
+
+	while (fscanf(buku_file, "%s", data_temp) != EOF){
+		if (strlen(data_temp) > 0){
+			strcpy(buku_data[i].path, strtok(data_temp, ":"));
+			strcpy(buku_data[i].publisher, strtok(NULL, ":"));
+			strcpy(buku_data[i].year, strtok(NULL, ":"));
+
+			printf("Buku:\npath : %s\npublisher : %s\nyear : %s\n\n", buku_data[i].path, buku_data[i].publisher, buku_data[i].year);
+
+			i++;
+		}
+	}
+
+	buku_data_size = i;
+
+	fclose(buku_file);
+
+	printf("Done Reading files.csv data.\n");
+}
+
+void read_akun_file(){
+	char data_temp[256] = {};
+
+	int i = 0;
+
+	akun_file = fopen("akun.txt", "r");
+
+	printf("Reading akun.txt data..\n");
+
+	while (fscanf(akun_file, "%s", data_temp) != EOF){
+		if (strlen(data_temp) > 0){
+			strcpy(akun_data[i].id, strtok(data_temp, ":"));
+			strcpy(akun_data[i].pass, strtok(NULL, ":"));
+
+			printf("%s -> %s\n", akun_data[i].id, akun_data[i].pass);
+
+			i++;
+		}
+	}
+
+	akun_data_size = i;
+
+	fclose(akun_file);
+
+	printf("Done Reading akun.txt data.\n");
+}
+
+void append_akun_file(char *id, char *pass){
+	akun_file = fopen("akun.txt", "a");
+
+	printf("Writing akun.txt data..\n");
+
+	fprintf(akun_file, "%s:%s\n", id, pass);
+
+	fclose(akun_file);
+
+	printf("Done Write akun.txt data.\n");
+}
+
+void login_akun(char *id, char *pass){
+	int i = 0;
+	for (;i < akun_data_size;i++){
+		if (strcmp(akun_data[i].id, id) == 0){
+			if (strcmp(akun_data[i].pass, pass) == 0){
+				logined_akun = &akun_data[i];
+				return;
+			}
+		}
+	}
+}
+
+void register_akun(char *id, char *pass){
+	strcpy(akun_data[akun_data_size].id, id);
+	strcpy(akun_data[akun_data_size].pass, pass);
+	akun_data_size++;
+	append_akun_file(id, pass);
+}
+
+void logout_akun(){
+	logined_akun = NULL;
 }
 
 void home_ui(char *s){
@@ -28,6 +139,10 @@ void goodbye_ui(char *s){
 }
 
 int main(int argc, char const *argv[]) {
+	//Preparation
+	read_akun_file();
+
+	//Untuk konek
 	int server_fd, new_socket, valread;
 	struct sockaddr_in address;
 	int opt = 1;
@@ -64,13 +179,15 @@ int main(int argc, char const *argv[]) {
 		exit(EXIT_FAILURE);
 	}
 
+	//Mulai konek
+
 	bool is_running = true;
 
 	valread = read( new_socket , buffer, 1024);
+	home_ui(sendbuffer);
+	send(new_socket, sendbuffer, 1024 ,0);
 
 	while (is_running){
-		home_ui(sendbuffer);
-		send(new_socket, sendbuffer, 1024 ,0);
 		valread = read( new_socket , buffer, 1024);
 
 		resetbuffer(sendbuffer);
@@ -81,15 +198,16 @@ int main(int argc, char const *argv[]) {
 		} else if (strcmp(buffer, "3") == 0){
 			sprintf(sendbuffer,"exit\n");
 			is_running = false;
+			break;
 		} else {
-			sprintf(sendbuffer,"command not right\n");
+			home_ui(sendbuffer);
+			sprintf(sendbuffer + strlen(sendbuffer),"command not right\n");
 		}
 
-		printf("%s\n", buffer);
 		send(new_socket, sendbuffer, 1024 ,0);
 	}
 
 	goodbye_ui(sendbuffer);
-	send(new_socket, sendbuffer, strlen(sendbuffer) ,0);
+	send(new_socket, sendbuffer, 1024 ,0);
 	return 0;
 }
